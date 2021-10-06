8794ab46b1105bd87a1caae7972509e1fe405431
sergey b <sbarchshevskiy@gmail.com>
2021-10-06 15:27:40 -0400

    fetch basic ariticle info, not the url yet

--- script.rb
+++ script.rb
@@ -7,7 +7,15 @@ def fetch
    url = 'https://www.lapresse.ca/actualites'
    rawData = HTTParty.get(url)
    selectedData = Nokogiri::HTML(rawData)
-   byebug	
+   headlines = selectedData.css('div.articleDetail')
+   headlines.each do |headline|
+      output = {
+        title: headline.css('span.title').text,
+        url: headline.css('a')[0].attributes['href'].text
+
+      }
+      byebug
+   end
 end
 
-fetch	
+fetch
